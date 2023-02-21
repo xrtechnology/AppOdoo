bd5ea7ec7b81155aeff0baa31db33525d6f3a741
xrtechnology <102915726+xrtechnology@users.noreply.github.com>
2023-02-20 23:12:54 -0400

    initial commit
    Version Final de App Gastos empleados Factura

---
+++ hr_expense_invoice/models/__init__.py
@@ -0,0 +1,6 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import hr_expense
+from . import hr_expense_sheet
+from . import account_move
+from . import account_move_line
